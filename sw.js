e40ffef12ee11e49234384f583b011887735591e
FurtiveDev <etibar.19732@gmail.com>
2026-03-31 20:46:21 +0300

    Revert to original, fix cache to always serve latest

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'reminder-pwa-v1';
+const CACHE_NAME = 'reminder-pwa-v2';
 const ASSETS = [
   '/',
   '/index.html',
@@ -31,13 +31,13 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(cached => cached || fetch(event.request))
-      .catch(() => {
-        if (event.request.mode === 'navigate') {
-          return caches.match('/index.html');
-        }
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return response;
       })
+      .catch(() => caches.match(event.request))
   );
 });
 
